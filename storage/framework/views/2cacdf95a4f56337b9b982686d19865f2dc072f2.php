ec05847acae6c59f52a5fff1c0c4ad648a9e4568
a7med404 <a7med_404@outlook.com>
2019-08-28 16:50:22 +0200

    set auth

--- storage/framework/views/2cacdf95a4f56337b9b982686d19865f2dc072f2.php
+++
@@ -1,9 +0,0 @@
-<!-- footer content -->
-<footer class="main-footer">
-  <div class="pull-left hidden-xs">
-  <b>Version</b> 1.0.0
-  </div>
-  <strong>Copyright &copy; 2019 <a href="http://a7med404.com">School System</a></strong> — Developed by <a href="https://www.iprismtech.com" target="_blank">iprismtech</a> Software. 
-</footer>
-<!-- /footer content -->
-<?php /**PATH /home/barca/fouad/projects/school-system_4/school-system/resources/views/cpanel/partials/footer.blade.php ENDPATH**/ ?>
